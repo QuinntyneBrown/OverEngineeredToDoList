9fec79d2e28220fb775410272763347167343763
Quinntyne Brown <quinntynebrown@gmail.com>
2022-05-29 11:53:49 -0400

    refactor

--- src/OverEngineeredToDoList.App/src/app/@shared/components/to-do-dialog/create-to-do-dialog-view-model.ts
+++ src/OverEngineeredToDoList.App/src/app/@shared/components/to-do-dialog/create-to-do-dialog-view-model.ts
@@ -11,20 +11,16 @@ export function createToDoDialogViewModel(save$: Observable<void>) {
     const dialogRef = inject(MatDialogRef);
     const toDo: ToDo = inject(MAT_DIALOG_DATA);
     const form = new FormGroup({
-      toDoId: new FormControl("",[]),
-      name: new FormControl("",[Validators.required]),
-      complete: new FormControl(false,[Validators.required])
+      toDoId: new FormControl(toDo.toDoId || "",[]),
+      name: new FormControl(toDo.name || "",[Validators.required]),
+      complete: new FormControl(toDo.complete || false,[Validators.required])
     });
-
-    if(toDo) {
-      form.patchValue(toDo)
-    }
 
     return save$.pipe(
       switchMap(_ => form.value.toDoId ? toDoService.updateToDo(form.value) : toDoService.createToDo(form.value)),
       map(response => {
         dialogRef.close(response.toDo);
-        return {form};
+        return { form };
       }),
       startWith(({ form }))
     );  
