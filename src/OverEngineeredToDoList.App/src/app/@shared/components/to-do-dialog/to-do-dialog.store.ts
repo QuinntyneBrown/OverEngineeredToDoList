33326ede26a1fe3d880ab91d0e984bd49a523f88
Quinntyne Brown <quinntynebrown@gmail.com>
2022-06-11 10:21:35 -0400

    refactor

--- src/OverEngineeredToDoList.App/src/app/@shared/components/to-do-dialog/to-do-dialog.store.ts
+++ src/OverEngineeredToDoList.App/src/app/@shared/components/to-do-dialog/to-do-dialog.store.ts
@@ -4,7 +4,7 @@ import { ToDoService } from "@api";
 import { ComponentStore, tapResponse } from "@ngrx/component-store";
 import { ToDo } from "@shared/models/to-do";
 import { ToDoStore } from "@shared/state/to-do.store";
-import { exhaustMap, switchMap, take } from "rxjs";
+import { exhaustMap, withLatestFrom } from "rxjs";
 
 @Injectable()
 export class ToDoDialogStore extends ComponentStore<null> {
@@ -15,25 +15,23 @@ export class ToDoDialogStore extends ComponentStore<null> {
 
     save = this.effect(
         exhaustMap((toDo:ToDo) => (toDo?.toDoId ? this._toDoService.updateToDo(toDo) : this._toDoService.createToDo(toDo)).pipe(
-            switchMap(response => this._toDoStore.state$.pipe(
-                take(1),
-                tapResponse(
-                    state => {
-                        const toDos = toDo.toDoId ? state.toDos.map(x => {
-                            if(toDo.toDoId == x.toDoId) {
-                                return response.toDo;
-                            }
-                            return x;
-                        }) : [...state.toDos, response.toDo];
+            withLatestFrom(this._toDoStore.state$),
+            tapResponse(
+                ([response,state]) => {
+                    const toDos = toDo.toDoId ? state.toDos.map(x => {
+                        if(toDo.toDoId == x.toDoId) {
+                            return response.toDo;
+                        }
+                        return x;
+                    }) : [...state.toDos, response.toDo];
 
-                        this._toDoStore.patchState({ toDos });
-                        this._dialogRef.close(null);
-                    },
-                    error => {
+                    this._toDoStore.patchState({ toDos });
+                    this._dialogRef.close(null);
+                },
+                error => {
 
-                    }
-                )
-            ))
+                }
+            )
         ))
-    )
+    )    
 }
