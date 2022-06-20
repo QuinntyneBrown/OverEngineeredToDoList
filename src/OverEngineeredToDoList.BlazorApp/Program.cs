0535249ab485d69eb5bc9f80eb6acc596d0a8f3a
QuinntyneBrown <quinntynebrown@gmail.com>
2022-06-19 21:54:47 -0400

    clean up

--- src/OverEngineeredToDoList.BlazorApp/Program.cs
+++ src/OverEngineeredToDoList.BlazorApp/Program.cs
@@ -1,6 +1,5 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
-using OverEngineeredToDoList.BlazorApp.Data;
 using MediatR;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,7 +7,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
 
 Dependencies.Configure(builder.Services);
 
